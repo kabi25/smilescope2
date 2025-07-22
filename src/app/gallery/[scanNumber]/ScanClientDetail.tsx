09855ae26641d4fab9bca881848ffc5a6ebdbca4
kabi25 <2015kabilan@gmail.com>
2025-07-22 08:55:03 +0800

    save

--- src/app/gallery/[scanNumber]/ScanClientDetail.tsx
+++
@@ -1,8 +0,0 @@
-// src/app/gallery/[scanNumber]/page.tsx
-import dynamic from 'next/dynamic';
-
-const ScanDetailClient = dynamic(() => import('./ScanDetailClient'), { ssr: false });
-
-export default function Page({ params }: { params: { scanNumber: string } }) {
-  return <ScanDetailClient scanNumber={params.scanNumber} />;
-}
